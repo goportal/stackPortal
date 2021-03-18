71337bd74fb74c1cc54f42a5ef0e6676a2f12de2
GoPortal <gabriel.o.portal@gmail.com>
2021-03-18 10:27:28 -0300

    Using Prettier

--- server.js
+++ server.js
@@ -1,68 +1,80 @@
-const http = require("http");
+const http = require('http');
 const express = require('express');
 const app = express();
-const {Kafka} = require('kafkajs');
+const { Kafka } = require('kafkajs');
 const mongo = require('mongodb');
-const { syncBuiltinESMExports } = require("module");
+const { syncBuiltinESMExports } = require('module');
 const mongoClient = mongo.MongoClient;
 
 // This should be implemented in a config file.
-const url = "mongodb://127.0.0.1:27017/";
+const url = 'mongodb://127.0.0.1:27017/';
 
 const kafka = new Kafka({
-    clientId: 'server',
-    brokers: ['localhost:9092']
+	clientId: 'server',
+	brokers: ['localhost:9092'],
 });
 
-const consumer = kafka.consumer({groupId:'test-group'});
+const consumer = kafka.consumer({ groupId: 'test-group' });
 
-let auxConsumer = async()=>{
-    await consumer.connect();
-    await consumer.subscribe({topic:'test-topic',fromBeginning:false});
+let auxConsumer = async () => {
+	await consumer.connect();
+	await consumer.subscribe({ topic: 'test-topic', fromBeginning: false });
 
-    await consumer.run({
-        eachMessage: async ({topic, partition, message}) => {
-            let timestamp = new Date().getHours()+":"+new Date().getMinutes()+":"+new Date().getSeconds();
-            let temperature = {temp:message.value.toString(),timestamp:timestamp};
+	await consumer.run({
+		eachMessage: async ({ topic, partition, message }) => {
+			let timestamp =
+				new Date().getHours() +
+				':' +
+				new Date().getMinutes() +
+				':' +
+				new Date().getSeconds();
+			let temperature = {
+				temp: message.value.toString(),
+				timestamp: timestamp,
+			};
 
-            mongoClient.connect(url,(err,db)=>{
-                if(err) throw err;
-            
-                let dbo = db.db('stackPortal');
-            
-                dbo.collection("temperatures").insertOne(temperature,(err,response)=>{
-                    if (err) throw err;
-                    console.log("New temperature added..." + temperature);
-                    db.close();
-                });
-            });
-        }
-    });
-}
+			mongoClient.connect(url, (err, db) => {
+				if (err) throw err;
+
+				let dbo = db.db('stackPortal');
+
+				dbo
+					.collection('temperatures')
+					.insertOne(temperature, (err, response) => {
+						if (err) throw err;
+						console.log('New temperature added...' + temperature);
+						db.close();
+					});
+			});
+		},
+	});
+};
 
 auxConsumer();
 
-app.get("/",(req,res)=>{
-    res.send("<h1>Servidor rodando !</h1>");
+app.get('/', (req, res) => {
+	res.send('<h1>Servidor rodando !</h1>');
 });
 
-app.get("/lastTemperature",(req,res)=>{
+app.get('/lastTemperature', (req, res) => {
+	mongoClient.connect(url, (err, db) => {
+		if (err) throw err;
+		let dbo = db.db('stackPortal');
+		let cursor = dbo
+			.collection('temperatures')
+			.find()
+			.limit(1)
+			.sort({ $natural: -1 });
 
-    mongoClient.connect(url,(err,db)=>{
-        if(err) throw err;
-        let dbo = db.db('stackPortal');
-        let cursor = dbo.collection('temperatures').find().limit(1).sort({ $natural : -1 });
-
-        cursor.toArray(function(err, results) {
-            if (err) throw err;
-            res.send(results);
-            console.log('%j', results);
-            db.close();
-        });
-    });
-
+		cursor.toArray(function (err, results) {
+			if (err) throw err;
+			res.send(results);
+			console.log('%j', results);
+			db.close();
+		});
+	});
 });
 
-http.createServer(app).listen(3000, ()=>{
-    console.log("Servidor rodando na porta local 3000");
+http.createServer(app).listen(3000, () => {
+	console.log('Servidor rodando na porta local 3000');
 });
--- utils/kafkaPublisher.js
+++ utils/kafkaPublisher.js
@@ -1,38 +1,35 @@
-const {Kafka} = require('kafkajs');
+const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
-    clientId:'Client01',
-    brokers:['localhost:9092']
+	clientId: 'Client01',
+	brokers: ['localhost:9092'],
 });
 
-let send = async()=>{
+let send = async () => {
+	const producer = kafka.producer();
 
-    const producer = kafka.producer();
+	await producer.connect();
 
-    await producer.connect();
+	let online = true;
+	let temperature = 35;
 
-    let online = true;
-    let temperature = 35;
-    
-    do{
-        
-        if(Math.floor(Math.random() * Math.floor(11)) %2 == 0){
-            temperature+=1;
-        }else{
-            temperature-=1;
-        }
+	do {
+		if (Math.floor(Math.random() * Math.floor(11)) % 2 == 0) {
+			temperature += 1;
+		} else {
+			temperature -= 1;
+		}
 
-        console.log("Sending "+temperature+"c temperature");
+		console.log('Sending ' + temperature + 'c temperature');
 
-        await producer.send({
-            topic: 'test-topic',
-            messages:[{value:temperature.toString()}]
-        });
+		await producer.send({
+			topic: 'test-topic',
+			messages: [{ value: temperature.toString() }],
+		});
 
-        await new Promise(resolve => setTimeout(resolve, 5000));
-    }while(online);
-
-    await producer.disconnect();
-}
+		await new Promise((resolve) => setTimeout(resolve, 5000));
+	} while (online);
+	await producer.disconnect();
+};
 
 send();
